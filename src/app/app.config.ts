c2153651f7392df4a6af17c51852cad977eb68b7
Giovanni Witulski <witulski.schule@gmail.com>
2024-12-22 17:23:37 +0100

    designed the landing page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {  } from '@angular/router';
 
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { firebaseConfig } from '../environments/firebase.config';
+import { firebaseConfig } from '../environments/environment';
 import { provideHttpClient } from '@angular/common/http';
 
 
--- src/app/main-content/above-the-fold/above-the-fold.component.ts
+++ src/app/main-content/above-the-fold/above-the-fold.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-above-the-fold',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss'
 })
 export class AboveTheFoldComponent {
-
+  texts: string[] = [
+    'Available for remote work',
+    '•',
+    'Frontend Developer',
+    '•',
+    'Based in Munich',
+    '•',
+    'Open to Work',
+    '•',
+    'Available for remote work',
+    '•',
+    'Frontend Developer',
+    '•',
+    'Based in Munich',
+    '•',
+    'Open to Work',
+    '•',
+  ];
 }
