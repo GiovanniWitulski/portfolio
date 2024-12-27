05a3ee2f997060347a68089990565b600e8835af
Giovanni Witulski <witulski.schule@gmail.com>
2024-12-27 01:04:34 +0100

    designed contact section and contact form

--- src/app/shared/contactform/contactform.component.ts
+++ src/app/shared/contactform/contactform.component.ts
@@ -2,25 +2,24 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
-
 @Component({
   selector: 'app-contactform',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './contactform.component.html',
-  styleUrl: './contactform.component.scss'
+  styleUrl: './contactform.component.scss',
 })
 export class ContactformComponent {
-
-  http = inject(HttpClient)
+  http = inject(HttpClient);
 
   contactData = {
-    name: "",
-    email: "",
-    message: ""
-  }
+    name: '',
+    email: '',
+    message: '',
+  };
 
   mailTest = true;
+  privacyPolicyChecked = false;
 
   post = {
     endPoint: 'https://deineDomain.de/sendMail.php',
@@ -34,11 +33,16 @@ export class ContactformComponent {
   };
 
   onSubmit(ngForm: NgForm) {
-    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
-      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+    if (
+      ngForm.submitted &&
+      ngForm.form.valid &&
+      this.privacyPolicyChecked &&
+      !this.mailTest
+    ) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
         .subscribe({
           next: (response) => {
-
             ngForm.resetForm();
           },
           error: (error) => {
@@ -47,8 +51,25 @@ export class ContactformComponent {
           complete: () => console.info('send post complete'),
         });
     } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
-
       ngForm.resetForm();
     }
+  }
+
+  ngAfterViewInit() {
+    let formContainers = document.querySelectorAll(
+      'form > div:not(.checkbox-container, .submit-btn-container)'
+    );
+
+    formContainers.forEach((container) => {
+      container.addEventListener('click', () => {
+        let inputElement = container.querySelector(
+          'input, textarea'
+        ) as HTMLElement;
+
+        if (inputElement) {
+          inputElement.focus();
+        }
+      });
+    });
   }
 }
