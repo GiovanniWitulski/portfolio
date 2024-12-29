db540a6eb0a99519006f386ba83e81e5f07452b3
Giovanni Witulski <witulski.schule@gmail.com>
2024-12-29 22:25:32 +0100

    added footer + slide cards for viewing projects

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -1,62 +1,91 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  ViewChildren,
+  QueryList,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
 import { ProjectCardComponent } from './project-card/project-card.component';
-import { Project } from "./../../interfaces/project";
+import { Project } from './../../interfaces/project';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [
-    ProjectCardComponent,
-    CommonModule,
-  ],
+  imports: [ProjectCardComponent, CommonModule],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrl: './portfolio.component.scss',
 })
-export class PortfolioComponent {
-    projects: Project[] = [
-      {
-        title: 'Join',
-        technologies: [
-          {name: 'Angular', imgSrc: 'icon/angular-small.png'}, 
-          {name: 'TypeScript', imgSrc: 'icon/typescript-small.png'}, 
-          {name: 'HTML', imgSrc: 'icon/html-small.png'}, 
-          {name: 'CSS', imgSrc: 'icon/css-small.png'}, 
-          {name: 'Firebase', imgSrc: 'icon/firebase-small.png'}, 
-        ],
-        imageUrl: 'img/skyrim-map.jpg',
-        description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.', 
-        liveDemoLink: 'Live Test',
-        gitHubLink: 'GitHub',
-        inDevelopment: false,
-      },
-      {
-        title: 'El Pollo Loco',
-        technologies: [
-          {name: 'JavaScript', imgSrc: 'icon/javascript-small.png'}, 
-          {name: 'HTML', imgSrc: 'icon/html-small.png'}, 
-          {name: 'CSS', imgSrc: 'icon/css-small.png'},
-        ],
-        imageUrl: 'img/skyrim-map.jpg',
-        description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.', 
-        liveDemoLink: 'Live Test',
-        gitHubLink: 'GitHub',
-        inDevelopment: false,
-      },
-      {
-        title: 'DABubble',
-        technologies: [
-          {name: 'Angular', imgSrc: 'icon/angular-small.png'}, 
-          {name: 'TypeScript', imgSrc: 'icon/typescript-small.png'}, 
-          {name: 'HTML', imgSrc: 'icon/html-small.png'}, 
-          {name: 'CSS', imgSrc: 'icon/css-small.png'}, 
-          {name: 'Firebase', imgSrc: 'icon/firebase-small.png'}, 
-        ],
-        imageUrl: 'img/skyrim-map.jpg',
-        description: 'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.', 
-        liveDemoLink: 'Live Test',
-        gitHubLink: 'GitHub',
-        inDevelopment: true,
-      },
-    ];
+export class PortfolioComponent implements AfterViewInit {
+  @ViewChildren(ProjectCardComponent)
+  projectCardComponents!: QueryList<ProjectCardComponent>;
+
+  projects: Project[] = [
+    {
+      title: 'Join',
+      technologies: [
+        { name: 'Angular', imgSrc: 'icon/angular-small.png' },
+        { name: 'TypeScript', imgSrc: 'icon/typescript-small.png' },
+        { name: 'HTML', imgSrc: 'icon/html-small.png' },
+        { name: 'CSS', imgSrc: 'icon/css-small.png' },
+        { name: 'Firebase', imgSrc: 'icon/firebase-small.png' },
+      ],
+      imageUrl: 'img/skyrim-map.jpg',
+      description:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      liveDemoLink: 'Live Test',
+      gitHubLink: 'GitHub',
+      inDevelopment: false,
+    },
+    {
+      title: 'El Pollo Loco',
+      technologies: [
+        { name: 'JavaScript', imgSrc: 'icon/javascript-small.png' },
+        { name: 'HTML', imgSrc: 'icon/html-small.png' },
+        { name: 'CSS', imgSrc: 'icon/css-small.png' },
+      ],
+      imageUrl: 'img/skyrim-map.jpg',
+      description:
+        'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      liveDemoLink: 'Live Test',
+      gitHubLink: 'GitHub',
+      inDevelopment: false,
+    },
+    {
+      title: 'DABubble',
+      technologies: [
+        { name: 'Angular', imgSrc: 'icon/angular-small.png' },
+        { name: 'TypeScript', imgSrc: 'icon/typescript-small.png' },
+        { name: 'HTML', imgSrc: 'icon/html-small.png' },
+        { name: 'CSS', imgSrc: 'icon/css-small.png' },
+        { name: 'Firebase', imgSrc: 'icon/firebase-small.png' },
+      ],
+      imageUrl: 'img/skyrim-map.jpg',
+      description:
+        'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.',
+      liveDemoLink: 'Live Test',
+      gitHubLink: 'GitHub',
+      inDevelopment: true,
+    },
+  ];
+
+  openedCardIndex: number | null = null;
+
+  ngAfterViewInit(): void {
+    console.log(this.projectCardComponents);
+  }
+
+  openCard(index: number) {
+    this.openedCardIndex = index;
+    // Finde die zu öffnende Projektkarte und setze showContainer auf true
+    const projectCard = this.projectCardComponents.toArray()[index];
+    if (projectCard) {
+      projectCard.showContainer = true;
+    }
+  }
+
+  closeCard() {
+    this.openedCardIndex = null;
+  }
 }
--- src/app/main-content/portfolio/project-card/project-card.component.ts
+++ src/app/main-content/portfolio/project-card/project-card.component.ts
@@ -1,18 +1,48 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Project } from './../../../interfaces/project';
-import { PortfolioComponent } from './../portfolio.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
-  imports: [
-    CommonModule,
-    PortfolioComponent
-  ],
+  imports: [CommonModule],
   templateUrl: './project-card.component.html',
-  styleUrl: './project-card.component.scss'
+  styleUrl: './project-card.component.scss',
 })
-export class ProjectCardComponent {
-  @Input() project!: Project; 
+export class ProjectCardComponent implements OnInit {
+  @Input() project!: Project;
+  @Input() projects: Project[] = [];
+  @Input() openedCardIndex: number | null = null;
+  @Output() cardClosed = new EventEmitter<void>();
+  @Output() cardOpened = new EventEmitter<void>();
+  showContainer = false;
+
+  ngOnInit(): void {
+    if (this.projects.length === 0) {
+      this.projects = [this.project];
+    }
+  }
+
+  openCard() {
+    this.showContainer = true;
+    this.cardOpened.emit();
+  }
+
+  closeContainer() {
+    this.showContainer = false;
+    this.cardClosed.emit();
+  }
+
+  nextCard() {
+    const currentProjectIndex = this.projects.indexOf(this.project);
+    const nextIndex = (currentProjectIndex + 1) % this.projects.length;
+    this.project = this.projects[nextIndex];
+  }
+
+  previousCard() {
+    const currentProjectIndex = this.projects.indexOf(this.project);
+    const previousIndex =
+      (currentProjectIndex - 1 + this.projects.length) % this.projects.length;
+    this.project = this.projects[previousIndex];
+  }
 }
