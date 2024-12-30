7f0dab17fd1b122ba18ae6f0e7ba3d7061f569af
Giovanni Witulski <witulski.schule@gmail.com>
2024-12-30 22:07:33 +0100

    Added error msgs to contact form

--- src/app/shared/contactform/contactform.component.ts
+++ src/app/shared/contactform/contactform.component.ts
@@ -1,3 +1,83 @@
+// import { HttpClient } from '@angular/common/http';
+// import { Component, inject } from '@angular/core';
+// import { FormsModule, NgForm } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-contactform',
+//   standalone: true,
+//   imports: [FormsModule],
+//   templateUrl: './contactform.component.html',
+//   styleUrl: './contactform.component.scss',
+// })
+// export class ContactformComponent {
+//   http = inject(HttpClient);
+
+//   contactData = {
+//     name: '',
+//     email: '',
+//     message: '',
+//   };
+
+//   mailTest = true;
+//   privacyPolicyChecked = false;
+
+//   post = {
+//     endPoint: 'https://deineDomain.de/sendMail.php',
+//     body: (payload: any) => JSON.stringify(payload),
+//     options: {
+//       headers: {
+//         'Content-Type': 'text/plain',
+//         responseType: 'text',
+//       },
+//     },
+//   };
+
+//   onSubmit(ngForm: NgForm) {
+//     if (
+//       ngForm.submitted &&
+//       ngForm.form.valid &&
+//       this.privacyPolicyChecked &&
+//       !this.mailTest
+//     ) {
+//       this.http
+//         .post(this.post.endPoint, this.post.body(this.contactData))
+//         .subscribe({
+//           next: (response) => {
+//             ngForm.resetForm();
+//           },
+//           error: (error) => {
+//             console.error(error);
+//           },
+//           complete: () => console.info('send post complete'),
+//         });
+//     } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+//       console.log('hat geklappt');
+
+//       ngForm.resetForm();
+//     } else {
+//       console.log('nope');
+//     }
+//   }
+
+//   ngAfterViewInit() {
+//     let formContainers = document.querySelectorAll(
+//       'form > div:not(.checkbox-container, .submit-btn-container)'
+//     );
+
+//     formContainers.forEach((container) => {
+//       container.addEventListener('click', () => {
+//         let inputElement = container.querySelector(
+//           'input, textarea'
+//         ) as HTMLElement;
+
+//         if (inputElement) {
+//           inputElement.focus();
+//         }
+//       });
+//     });
+//   }
+// }
+
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
@@ -18,11 +98,19 @@ export class ContactformComponent {
     message: '',
   };
 
+  namePlaceholder = 'Your name goes here';
+  emailPlaceholder = 'youremail@email.com';
+  messagePlaceholder = 'Hello Giovanni, I am interested in...';
+
+  nameErrorText = 'Oops! it seems your name is missing';
+  emailErrorText = 'Hoppla! your email is required';
+  messageErrorText = 'What do you need to develop?';
+
   mailTest = true;
   privacyPolicyChecked = false;
 
   post = {
-    endPoint: 'https://deineDomain.de/sendMail.php',
+    endPoint: 'https://giovanni-witulski.com/sendMail.php',
     body: (payload: any) => JSON.stringify(payload),
     options: {
       headers: {
@@ -51,7 +139,10 @@ export class ContactformComponent {
           complete: () => console.info('send post complete'),
         });
     } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      console.log('hat geklappt');
       ngForm.resetForm();
+    } else {
+      console.log('nope');
     }
   }
 
